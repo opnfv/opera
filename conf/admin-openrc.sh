96d5b325b54411d2aae5cee1e1fc79fc397cdfb1
Harry Huang <huangxiangyu5@huawei.com>
2017-01-10 16:11:55 +0800

    Opera deploy scripts
    JIRA: OPERA-2
    
    Function:
      1. Launch a VM (defaultly CentOS 7)
      2. Launch Open-O dockers inside VM
      3. Launch juju inside openstack (not finished)
    
    Directory conf/ need config according to local environment
    
    Change-Id: Iac24051668e15b60f76f4819760f7b7c9b1867f3
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

---
+++ conf/admin-openrc.sh
@@ -0,0 +1,19 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+# Verify the Identity Service installation
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_TENANT_NAME=admin
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=console
+export OS_AUTH_URL=http://172.16.1.222:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+export OS_REGION_NAME=RegionOne
