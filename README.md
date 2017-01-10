f9bd42230f1cd639a4f0787776974ad60e300d2b
Harry Huang <huangxiangyu5@huawei.com>
2017-01-10 16:11:55 +0800

    Opera deploy scripts
    JIRA: OPERA-2
    
    Requirement:
      After compass4nfv deployment
    
    Function:
      1. Launch a VM (defaultly CentOS 7)
      2. Launch Open-O dockers inside VM
      3. Launch juju inside openstack
    
    Directory conf/ need config according to local environment
    
    Change-Id: Iac24051668e15b60f76f4819760f7b7c9b1867f3
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 OPNFV OPEN-O Integration Source Codes
+
+Code Structure:
+opera/
+├── ci
+│   └── deploy.sh
+├── conf
+│   ├── admin-openrc.sh # compass openrc file
+│   ├── juju.conf # juju config
+│   ├── openo-docker.conf # open-o docker config
+│   └── openo-vm.conf # open-o vm config
+├── INFO
+├── juju
+│   ├── juju_launch.sh # launch juju vms
+│   └── juju_setup.sh # juju prepare 
+├── LICENSE
+├── model
+│   ├── bind.csar
+│   ├── bono.csar
+│   ├── ellis.csar
+│   ├── homer.csar
+│   ├── homestead.csar
+│   ├── proxy_node.csar
+│   ├── ralf.csar
+│   ├── sprout.csar
+│   └── vIMS_NS.csar
+├── open-o
+│   ├── openo_docker.sh # launch open-o docker inside open-o vm
+│   └── openo_vm.sh # launch open-o vm
+├── opera_launch.sh # opera deploy entrance
+├── prepare.sh # prepare work for opera
+├── README.md
+└── util
+    ├── isolinux.cfg # kickstart open-o vm
+    ├── ks.cfg # kickstart open-o vm
+    ├── libvirt.xml # define open-o vm
+    └── log.sh # different log print
+
---
+++ open-o/openo_docker.sh
@@ -0,0 +1,229 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016-2017 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+function docker_pull()
+{
+    until docker pull openoint/sdno-driver-ct-te
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-auth
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-drivermanager
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-extsys
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-msb
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-protocolstack
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-services-wso2ext
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-tosca-catalog
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-tosca-inventory
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-tosca-modeldesigner
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/gso-service-gateway
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/gso-service-manager
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-dac
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-driver-sdnc-zte
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-driver-vim
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-driver-vnfm-huawei
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-driver-vnfm-juju
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-driver-vnfm-zte
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-lcm
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-resmanagement
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/nfvo-umc
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-driver-huawei-l3vpn
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-driver-huawei-openstack
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-driver-huawei-overlay
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-driver-huawei-servicechain
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-driver-zte-sptn
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-brs
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-ipsec
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-l2vpn
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-l3vpn
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-mss
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-nslcm
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-overlayvpn
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-servicechain
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-vpc
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-service-vxlan
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/common-tosca-aria
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-monitoring
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/sdno-vsitemgr
+    do
+        echo "Try again"
+    done
+    until docker pull openoint/gso-gui-portal
+    do
+        echo "Try again"
+    done
+}
+
+function docker_run()
+{
+    docker run -d -e MODEL_DESIGNER_IP=COMMON_TOSCA_MODELDESIGNER_IP --network lab_net --ip COMMON_SERVICES_MSB_IP --name common-services-msb openoint/common-services-msb
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --add-host controller:127.0.0.1 --network lab_net --ip COMMON_SERVICES_AUTH_IP --name common-services-auth openoint/common-services-auth
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_SERVICES_DRIVERMANAGER_IP --name common-services-drivermanager openoint/common-services-drivermanager
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_SERVICES_EXTSYS_IP --name common-services-extsys openoint/common-services-extsys
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_SERVICES_PROTOCOLSTACK_IP --name common-services-protocolstack openoint/common-services-protocolstack
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_SERVICES_WSO2EXT_IP --name common-services-wso2ext openoint/common-services-wso2ext
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_TOSCA_CATALOG_IP --name common-tosca-catalog openoint/common-tosca-catalog
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_TOSCA_INVENTORY_IP --name common-tosca-inventory openoint/common-tosca-inventory
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_TOSCA_MODELDESIGNER_IP --name common-tosca-modeldesigner openoint/common-tosca-modeldesigner
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip GSO_SERVICE_GATEWAY_IP --name gso-service-gateway openoint/gso-service-gateway
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 -e MYSQL_ADDR=COMMON_TOSCA_INVENTORY_IP:3306 --network lab_net --ip GSO_SERVICE_MANAGER_IP --name gso-service-manager openoint/gso-service-manager
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DAC_IP --name nfvo-dac openoint/nfvo-dac
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DRIVER_SDNC_ZTE_IP --name nfvo-driver-sdnc-zte openoint/nfvo-driver-sdnc-zte
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DRIVER_VIM_IP --name nfvo-driver-vim openoint/nfvo-driver-vim
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DRIVER_VNFM_HUAWEI_IP --name nfvo-driver-vnfm-huawei openoint/nfvo-driver-vnfm-huawei
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DRIVER_VNFM_JUJU_IP --name nfvo-driver-vnfm-juju openoint/nfvo-driver-vnfm-juju
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_DRIVER_VNFM_ZTE_IP --name nfvo-driver-vnfm-zte openoint/nfvo-driver-vnfm-zte
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 -e MYSQL_ADDR=COMMON_TOSCA_INVENTORY_IP:3306 --network lab_net --ip NFVO_LCM_IP --name nfvo-lcm openoint/nfvo-lcm
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_RESMANAGEMENT_IP --name nfvo-resmanagement openoint/nfvo-resmanagement
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip NFVO_UMC_IP --name nfvo-umc openoint/nfvo-umc
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_HUAWEI_L3VPN_IP --name sdno-driver-huawei-l3vpn openoint/sdno-driver-huawei-l3vpn
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_HUAWEI_OPENSTACK_IP --name sdno-driver-huawei-openstack openoint/sdno-driver-huawei-openstack
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_HUAWEI_OVERLAY_IP --name sdno-driver-huawei-overlay openoint/sdno-driver-huawei-overlay
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_HUAWEI_SERVICECHAIN_IP --name sdno-driver-huawei-servicechain openoint/sdno-driver-huawei-servicechain
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_ZTE_SPTN_IP --name sdno-driver-zte-sptn openoint/sdno-driver-zte-sptn
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_BRS_IP --name sdno-service-brs openoint/sdno-service-brs
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_IPSEC_IP --name sdno-service-ipsec openoint/sdno-service-ipsec
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_L2VPN_IP --name sdno-service-l2vpn openoint/sdno-service-l2vpn
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_L3VPN_IP --name sdno-service-l3vpn openoint/sdno-service-l3vpn
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_MSS_IP --name sdno-service-mss openoint/sdno-service-mss
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 -e MYSQL_ADDR=COMMON_TOSCA_INVENTORY_IP:3306 --network lab_net --ip SDNO_SERVICE_NSLCM_IP --name sdno-service-nslcm openoint/sdno-service-nslcm
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_OVERLAYVPN_IP --name sdno-service-overlayvpn openoint/sdno-service-overlayvpn
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_SERVICECHAIN_IP --name sdno-service-servicechain openoint/sdno-service-servicechain
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_VPC_IP --name sdno-service-vpc openoint/sdno-service-vpc
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_SERVICE_VXLAN_IP --name sdno-service-vxlan openoint/sdno-service-vxlan
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip COMMON_TOSCA_ARIA_IP --name common-tosca-aria openoint/common-tosca-aria
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_DRIVER_CT_TE_IP --name sdno-driver-ct-te openoint/sdno-driver-ct-te
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_MONITORING_IP --name sdno-monitoring openoint/sdno-monitoring
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip SDNO_VSITEMGR_IP --name sdno-vsitemgr openoint/sdno-vsitemgr
+    docker run -d -e MSB_ADDR=COMMON_SERVICES_MSB_IP:80 --network lab_net --ip GSO_GUI_PORTAL_IP --name gso-gui-portal openoint/gso-gui-portal
+}
+
+yum update -y
+yum install -y curl net-tools
+curl -sSL https://experimental.docker.com/ | sh
+service docker start
+
+docker network create -d macvlan --subnet=OPENO_EXTERNAL_CIDR --gateway=OPENO_EXTERNAL_GW -o parent=eth0 lab_net
+docker_pull
+docker_run
+
+if [[ $(docker ps -aq | wc -l) == 40 ]];then
+    echo -e "\n\033[32mOpen-O Installed!\033[0m\n"
+fi
