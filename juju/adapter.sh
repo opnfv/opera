5b6c84722ca9f6d123c1b20600206716cbbdb3ca
Harry Huang <huangxiangyu5@huawei.com>
2017-01-10 16:11:55 +0800

    Opera deploy scripts
    JIRA: OPERA-2
    
    Requirement:
      After compass4nfv deployment
    
    Function:
      1. Launch a VM (defaultly CentOS 7)
      2. Launch Open-O dockers inside VM
      3. Launch juju inside openstack
    
    Directory conf/ need config according to local environment
    
    Change-Id: Iac24051668e15b60f76f4819760f7b7c9b1867f3
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

---
+++ juju/adapter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+function generate_compass_openrc()
+{
+    ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+    MGMT_IP=192.168.200.2
+    exec_command="sshpass -p root ssh $ssh_options root@$MGMT_IP"
+
+    RUN_DIR=$(eval "$exec_command ls /var/ansible/run")
+
+    cmd="cat /var/ansible/run/$RUN_DIR/group_vars/all | grep -A 3 public_vip: | sed -n '2p' | sed -e 's/  ip: //g'"
+    PUBLIC_VIP=$(eval "$exec_command $cmd")
+
+    echo -e "export OS_PASSWORD=console \n\
+export OS_TENANT_NAME=admin \n\
+export OS_AUTH_URL=http://$PUBLIC_VIP:35357/v2.0 \n\
+export OS_USERNAME=admin \n\
+export OS_VOLUME_API_VERSION=2 \n\
+export OS_REGION_NAME=RegionOne " > $WORK_DIR/admin-openrc.sh
+
+}
