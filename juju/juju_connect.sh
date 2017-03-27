5a9cd98783bf7547cb56b3da84521315d3354f21
Harry Huang <huangxiangyu5@huawei.com>
2017-03-27 11:53:39 +0800

    run without sudo
    Jenkins can't remove some dir generate by sudo
    and leads to CI error for following tasks.
    
    Change codes to run ./opera_launch.sh without
    sudo
    
    Change-Id: Iaebd7e1fbdf413a91cc55c77e1b5c6915cfc86d6
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>
    (cherry picked from commit 7ee5c95db62d13d860d51974aa9c0a14404cee56)

--- juju/juju_connect.sh
+++ juju/juju_connect.sh
@@ -10,7 +10,7 @@
 
 function connect_prepare()
 {
-    apt-get install -y rsync
+    sudo apt-get install -y rsync
 
     local cmd="tar -zxvf apache-tomcat-8.5.9.tar.gz; \
                sudo rm -rf tomcat8 csar; \
@@ -23,50 +23,52 @@ function connect_prepare()
 function sync_juju_driver_file()
 {
     local TOMCAT_DIR=${WORK_DIR}/tomcat
-    rm -rf ${TOMCAT_DIR}
+    sudo rm -rf ${TOMCAT_DIR}
     mkdir -p ${TOMCAT_DIR}
 
     connect_prepare
 
-    docker cp nfvo-driver-vnfm-juju:/service/webapps/ROOT ${TOMCAT_DIR}
-    docker cp nfvo-driver-vnfm-juju:/service/etc ${TOMCAT_DIR}
+    sudo docker cp nfvo-driver-vnfm-juju:/service/webapps/ROOT ${TOMCAT_DIR}
+    sudo docker cp nfvo-driver-vnfm-juju:/service/etc ${TOMCAT_DIR}
 
     file1=${TOMCAT_DIR}/etc/conf/juju_conf.json
-    sed -i "s/^\(.*\"image-metadata-url\":\).*/\1 \"http:\/\/$juju_client_ip\/images\"\,/g" $file1
-    sed -i "s/^\(.*\"network\":\).*/\1 \"juju-net\"\,/g" $file1
-    sed -i "s/^\(.*\"use-floating-ip\":\).*/\1 \"True\"\,/g" $file1
+    sudo sed -i "s/^\(.*\"image-metadata-url\":\).*/\1 \"http:\/\/$juju_client_ip\/images\"\,/g" $file1
+    sudo sed -i "s/^\(.*\"network\":\).*/\1 \"juju-net\"\,/g" $file1
+    sudo sed -i "s/^\(.*\"use-floating-ip\":\).*/\1 \"True\"\,/g" $file1
 
     file2=${TOMCAT_DIR}/etc/csarInfo/csarinfo.json
-    sed -i "s/^\(.*\"csar_file_path\":\).*/\1 \"\/home\/ubuntu\/csar\/\"\,/g" $file2
+    sudo sed -i "s/^\(.*\"csar_file_path\":\).*/\1 \"\/home\/ubuntu\/csar\/\"\,/g" $file2
 
     file3=${TOMCAT_DIR}/ROOT/WEB-INF/classes/db.properties
-    sed -i "s/^\(.*jdbc.url=\).*/\1jdbc:mysql:\/\/$OPENO_IP:$NFVO_DRIVER_VNFM_JUJU_MYSQL_PORT\/jujuvnfmdb/g" $file3
+    sudo sed -i "s/^\(.*jdbc.url=\).*/\1jdbc:mysql:\/\/$OPENO_IP:$NFVO_DRIVER_VNFM_JUJU_MYSQL_PORT\/jujuvnfmdb/g" $file3
 
     file4=${TOMCAT_DIR}/ROOT/WEB-INF/classes/juju-config.properties
-    sed -i "s/^\(.*charmPath=\).*/\1\/home\/ubuntu\/csar\//g" $file4
-    sed -i "s/^\(.*grant_jujuvnfm_url=\).*/\1http:\/\/$OPENO_IP:$NFVO_DRIVER_VNFM_JUJU_PORT\//g" $file4
+    sudo sed -i "s/^\(.*charmPath=\).*/\1\/home\/ubuntu\/csar\//g" $file4
+    sudo sed -i "s/^\(.*grant_jujuvnfm_url=\).*/\1http:\/\/$OPENO_IP:$NFVO_DRIVER_VNFM_JUJU_PORT\//g" $file4
 
     file5=${TOMCAT_DIR}/etc/conf/restclient.json
-    sed -i "s/^\(.*\"host\":\).*/\1\"$OPENO_IP\"\,/g" $file5
-    sed -i "s|^\(.*\"port\":\).*|\1\"$COMMON_SERVICES_MSB_PORT\"|g" $file5
+    sudo sed -i "s/^\(.*\"host\":\).*/\1\"$OPENO_IP\"\,/g" $file5
+    sudo sed -i "s|^\(.*\"port\":\).*|\1\"$COMMON_SERVICES_MSB_PORT\"|g" $file5
 
     file6=${TOMCAT_DIR}/etc/adapterInfo/jujuadapterinfo.json
-    sed -i "s/^\(.*\"ip\":\).*/\1 \"$OPENO_IP\"\,/g" $file6
+    sudo sed -i "s/^\(.*\"ip\":\).*/\1 \"$OPENO_IP\"\,/g" $file6
 
     rsync -e 'ssh -o StrictHostKeyChecking=no' --rsync-path='sudo rsync' \
     -av ${TOMCAT_DIR}/etc ubuntu@$juju_client_ip:/home/ubuntu/tomcat8/
     rsync -e 'ssh -o StrictHostKeyChecking=no' --rsync-path='sudo rsync' \
     -av ${TOMCAT_DIR}/ROOT ubuntu@$juju_client_ip:/home/ubuntu/tomcat8/webapps
 
-    docker cp ${TOMCAT_DIR}/etc nfvo-driver-vnfm-juju:/service/
-    docker cp ${TOMCAT_DIR}/ROOT nfvo-driver-vnfm-juju:/service/webapps/
+    sudo docker cp ${TOMCAT_DIR}/etc nfvo-driver-vnfm-juju:/service/
+    sudo docker cp ${TOMCAT_DIR}/ROOT nfvo-driver-vnfm-juju:/service/webapps/
+
+    sudo rm -rf ${TOMCAT_DIR}
 }
 
 function start_tomcat()
 {
     chmod +x ${UTIL_DIR}/grant_mysql.sh
-    docker cp ${UTIL_DIR}/grant_mysql.sh nfvo-driver-vnfm-juju:/service
-    docker exec -i nfvo-driver-vnfm-juju /service/grant_mysql.sh
+    sudo docker cp ${UTIL_DIR}/grant_mysql.sh nfvo-driver-vnfm-juju:/service
+    sudo docker exec -i nfvo-driver-vnfm-juju /service/grant_mysql.sh
 
     local cmd1='sed -i s/port=\"8080\"/port=\"8483\"/g /home/ubuntu/tomcat8/conf/server.xml'
     exec_cmd_on_client $cmd1
@@ -76,8 +78,8 @@ function start_tomcat()
 
     exec_cmd_on_client $cmd2
 
-    docker stop nfvo-driver-vnfm-juju
-    docker start nfvo-driver-vnfm-juju
+    sudo docker stop nfvo-driver-vnfm-juju
+    sudo docker start nfvo-driver-vnfm-juju
     sleep 10
 }
 
@@ -101,12 +103,13 @@ function deploy_vnf()
 
 function fix_openo_containers()
 {
-    docker exec gso-service-gateway sed -i "s|^\(.*\"port\":\).*|\1 \"$COMMON_SERVICES_MSB_PORT\"|g" /service/etc/conf/restclient.json
-    docker stop gso-service-gateway
-    docker start gso-service-gateway
-    docker exec nfvo-resmanagement sed -i "s|^\(.*\"port\":\).*|\1 \"$COMMON_SERVICES_MSB_PORT\"|g" /service/etc/conf/restclient.json
-    docker stop nfvo-resmanagement
-    docker start nfvo-resmanagement
+    sudo docker exec gso-service-gateway sed -i "s|^\(.*\"port\":\).*|\1 \"$COMMON_SERVICES_MSB_PORT\"|g" /service/etc/conf/restclient.json
+    sudo docker stop gso-service-gateway
+    sudo docker start gso-service-gateway
+    sudo docker exec nfvo-resmanagement sed -i "s|^\(.*\"port\":\).*|\1 \"$COMMON_SERVICES_MSB_PORT\"|g" /service/etc/conf/restclient.json
+    sudo docker stop nfvo-resmanagement
+    sudo docker start nfvo-resmanagement
+    sleep 10
 }
 
 function connect_juju_and_openo()
